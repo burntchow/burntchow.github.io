2b639f867fa48f63f280fb88b3c540c4498af405
ave <72708879+burntchow@users.noreply.github.com>
2024-08-15 15:10:26 -0700

    icon setup

---
+++ script.js
@@ -0,0 +1,16 @@
+function openFolder(folderName) {
+    switch(folderName) {
+        case 'About':
+            window.location.href = 'about.html';
+            break;
+        case 'Projects':
+            window.location.href = 'projects.html';
+            break;
+        case 'GitHub':
+            window.location.href = 'https://github.com/burntchow';
+            break;
+        case 'LinkedIn':
+            window.location.href = 'https://www.linkedin.com/in/aveline-villaganas/';
+            break;
+    }
+}
